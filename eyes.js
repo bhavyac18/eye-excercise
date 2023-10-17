2a8f5b96f6154e4080d4753f4c3661aee0f289bd
Bhavyasree Chethil <53191128+bhavyac18@users.noreply.github.com>
2023-10-17 21:31:55 +0800

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -1,12 +1,19 @@
 var balls = document.getElementsByClassName("ball");
 var turnOff = document.getElementsByClassName("btn");
+var turnOn = document.getElementsByClassName("btn1");
 
 turnOff[0].addEventListener("click", function (event) {
   event.preventDefault();
   document.onmousemove = null;
 });
+turnOn[0].addEventListener("click", function (event) {
+  event.preventDefault();
+  document.onmousemove = handleMouseMove;
+});
 
-document.onmousemove = () => {
+document.onmousemove = handleMouseMove;
+
+function handleMouseMove(event) {
   var x = (event.clientX * 100) / window.innerWidth + "%";
   var y = (event.clientY * 100) / window.innerHeight + "%";
   for (let i = 0; i < balls.length; i++) {
@@ -14,4 +21,4 @@ document.onmousemove = () => {
     balls[i].style.top = y;
     balls[i].transfoorm = "translate(-" + x + ",-" + y + ")";
   }
-};
+}
